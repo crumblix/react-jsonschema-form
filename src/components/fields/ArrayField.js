3da0ee76b0ec567480488032b0e32c0e385b0e8f
EDORAS\mark <mark.jeacocke@gmail.com>
2017-01-26 16:43:00 +1000

    Revert changes to ArrayField ... unnecessary for lastEdit (for our purposes) and also created bugs.

--- src/components/fields/ArrayField.js
+++ src/components/fields/ArrayField.js
@@ -84,9 +84,9 @@ class ArrayField extends Component {
     return itemsSchema.type === "string" && itemsSchema.minLength > 0;
   }
 
-  asyncSetState(state, options={validate: false}, name) {
+  asyncSetState(state, options={validate: false}) {
     setState(this, state, () => {
-      this.props.onChange(this.state.items, options, name);
+      this.props.onChange(this.state.items, options);
     });
   }
 
@@ -102,7 +102,7 @@ class ArrayField extends Component {
     this.asyncSetState({
       items: items.concat([
         getDefaultFormState(itemSchema, undefined, definitions)
-      ],undefined,this.props.name)
+      ])
     });
   };
 
@@ -120,7 +120,7 @@ class ArrayField extends Component {
       }
       this.asyncSetState({
         items: newitems
-      }, {validate: true},this.props.name); // refs #195
+      }, {validate: true}); // refs #195
     };
   };
 
@@ -139,7 +139,7 @@ class ArrayField extends Component {
             return item;
           }
         })
-      }, {validate: true},this.props.name);
+      }, {validate: true});
     };
   };
 
@@ -149,12 +149,12 @@ class ArrayField extends Component {
         items: this.state.items.map((item, i) => {
           return index === i ? value : item;
         })
-      },undefined,this.props.name);
+      });
     };
   };
 
   onSelectChange = (value) => {
-    this.asyncSetState({items: value},undefined,this.props.name);
+    this.asyncSetState({items: value});
   };
 
   render() {
